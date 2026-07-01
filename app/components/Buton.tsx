c76dd9e49499b80f935f6c57eff7a61b01101d3d
zhangzhaosen <zhangzhaosen@sina.com>
2026-07-01 09:48:37 +0800

    feat: tailwind-merge & clsx

--- app/components/Buton.tsx
+++ app/components/Buton.tsx
@@ -1,13 +1,15 @@
 import { ComponentProps, ReactNode } from "react";
-
+import { twMerge } from "tailwind-merge";
+import { cn } from "../lib/utils";
 
 type ButtonProps = {
   children: ReactNode
 } & ComponentProps<'button'>
-export default function Button({ children, ...props }: ButtonProps) {
+export default function Button({ children, className, ...props }: ButtonProps) {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded
-    font-bold"
+    <button 
+    className={cn("bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded font-bold"
+      , className)} 
     {...props}
     >
       {children}
